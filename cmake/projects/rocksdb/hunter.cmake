c817491f73f4b90da2d7489e9702db92285723a8
hwaan2 <hwaan2@xcurenet.com>
2018-12-26 15:15:48 +0900

    오타 수정

--- cmake/projects/rocksdb/hunter.cmake
+++ cmake/projects/rocksdb/hunter.cmake
@@ -28,8 +28,8 @@ hunter_add_version(
     URL "https://github.com/facebook/rocksdb/archive/v5.14.2.tar.gz"
     SHA1 "376bc537e8c716111214274755f5fe4026e5a354")
 
-hunter_add_version(rocksdb
-    PACKAGE_NAME
+hunter_add_version(
+    PACKAGE_NAME rocksdb
     VERSION 5.17.4-h0
     URL "https://github.com/hwaan2/rocksdb/archive/v5.17.4-h0.tar.gz"
     SHA1 "a49b214790eb78fc8639a36bc35707ed99b451e4")
